d9bb21e48b9ac200caa12bf61ec9c802a18011c5
emzhran <zachran750@gmail.com>
2024-11-20 20:12:28 +0700

    menambahkan button

--- app/src/main/java/com/example/latihanmvvm/ui/view/screen/RencanaStudy.kt
+++ app/src/main/java/com/example/latihanmvvm/ui/view/screen/RencanaStudy.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -134,6 +135,18 @@ fun RencanaStudy(
                 )
                 Text(text = "Saya menyetujui setiap pernyataan yang ada tanpa ada paksaan dari pihak manapun.",
                     fontWeight = FontWeight.Light, fontSize = 10.sp)
+            }
+            Spacer(modifier = Modifier.padding(8.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ){
+                Button(onClick = {onBackButtonClicked()}) {
+                    Text(text = "Kembali")
+                }
+                Button(onClick = {onSubmitButtonClicked(listData)}, enabled = checked) {
+                    Text(text = "Lanjut")
+                }
             }
         }
     }
