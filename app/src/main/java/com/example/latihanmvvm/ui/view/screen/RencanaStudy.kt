a33cebfa0332ef4b493c2f7feee0a5e95c24e0b0
emzhran <zachran750@gmail.com>
2024-11-20 19:27:37 +0700

    menambahkan column yang berisi tampilan detail mahasiswa dan menambahkan box yang berisi ikon notifikasi

--- app/src/main/java/com/example/latihanmvvm/ui/view/screen/RencanaStudy.kt
+++ app/src/main/java/com/example/latihanmvvm/ui/view/screen/RencanaStudy.kt
@@ -2,13 +2,19 @@ package com.example.latihanmvvm.ui.view.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,9 +23,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.latihanmvvm.R
 import com.example.latihanmvvm.model.Mahasiswa
 
@@ -47,6 +56,22 @@ fun RencanaStudy(
                 modifier = Modifier
                     .clip(shape = CircleShape)
                     .size(50.dp))
+        }
+        Spacer(modifier = Modifier.padding(start = 16.dp))
+        Column(modifier = Modifier.weight(1f)) {
+            Text(text = mahasiswa.nama,
+                fontWeight = FontWeight.Bold,
+                fontSize = 15.sp,
+                color = Color.White)
+            Text(text = mahasiswa.nim,
+                fontWeight = FontWeight.Light,
+                fontSize = 12.sp,
+                color = Color.White)
+        }
+        Box(){
+            Icon(imageVector = Icons.Filled.Notifications,
+                contentDescription = "",
+                tint = Color.White)
         }
     }
 }
