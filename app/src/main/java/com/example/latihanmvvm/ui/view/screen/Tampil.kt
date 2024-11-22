e27f62064481f1e897b32a7290b16aabdf447a9f
emzhran <zachran750@gmail.com>
2024-11-22 14:17:21 +0700

    menambahkan tampilan teks KRS MAHASISWA dan Universitas Muhammadiyah Yogyakarta

--- app/src/main/java/com/example/latihanmvvm/ui/view/screen/Tampil.kt
+++ app/src/main/java/com/example/latihanmvvm/ui/view/screen/Tampil.kt
@@ -8,12 +8,18 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.latihanmvvm.R
 import com.example.latihanmvvm.model.Mahasiswa
 import com.example.latihanmvvm.model.RencanaStudi
@@ -36,6 +42,18 @@ fun Tampil(
             verticalAlignment = Alignment.CenterVertically) {
             Image(painter = painterResource(id = R.drawable.umy), contentDescription = "",
                 modifier = Modifier.size(40.dp))
+            Spacer(modifier = Modifier.padding(start = 14.dp))
+            Column {
+                Text(text = "KRS MAHASISWA",
+                    color = Color.White,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(text = "Universitas Muhammadiyah Yogyakarta",
+                    color = Color.White,
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Bold)
+            }
         }
     }
 }
